deda65edb29de9274456a45a118c1db0b15b7e57
chinobv <retrochanel97@gmail.com>
2019-11-29 17:13:27 -0600

    Updates

---
+++ precache-manifest.81a24663d371d62fb93f537533b7f649.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22188b5a3eae05b54411140d2b5dc020",
+    "url": "/app-news/index.html"
+  },
+  {
+    "revision": "076a21d43a4bc615ca34",
+    "url": "/app-news/static/css/main.67c93bfa.chunk.css"
+  },
+  {
+    "revision": "0add55c28c861c272955",
+    "url": "/app-news/static/js/2.d4575b75.chunk.js"
+  },
+  {
+    "revision": "076a21d43a4bc615ca34",
+    "url": "/app-news/static/js/main.b90b4940.chunk.js"
+  },
+  {
+    "revision": "dc61110b308a4be7a313",
+    "url": "/app-news/static/js/runtime-main.c4bcaf8f.js"
+  }
+]);
